0bd2d55fd6b11c48ea1183cbf104821ef51f179d
Sanjay Barman <sanbarma@publicisgroupe.net>
2022-07-04 16:28:02 +0530

    code refactor 101

--- src/app/route4/timer-menu/timer-menu.component.ts
+++ src/app/route4/timer-menu/timer-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ITimerData } from '../timer.model';
 import { TimerService } from '../timer.service';
@@ -6,34 +6,36 @@ import { TimerService } from '../timer.service';
 @Component({
   selector: 'route4-timer-menu',
   templateUrl: './timer-menu.component.html',
-  styleUrls: ['./timer-menu.component.scss']
+  styleUrls: ['./timer-menu.component.scss'],
 })
 export class TimerMenuComponent {
   timerControl = new FormControl();
   timerData: ITimerData[] = [];
   currentTimer;
-
   timerC: number;
 
   constructor(private timerService: TimerService) {}
 
   onStartOrPause(): void {
     const len = this.timerData.length;
-    if(
+    if (
       this.timerControl.value ||
       (this.timerData.length &&
-      !this.timerData[len-1].isActive &&
-      this.timerControl.value)
-      ) {
+        !this.timerData[len - 1].isActive &&
+        this.timerControl.value)
+    ) {
       this.timerData.push({
         limit: this.timerControl.value,
         isActive: true,
-        startTime:  new Date().toLocaleDateString('en-US') +' '+ new Date().toLocaleTimeString('en-US')
-      })
-      this.timerService.updateTimerData(this.timerData)
+        startTime:
+          new Date().toLocaleDateString('en-US') +
+          ' ' +
+          new Date().toLocaleTimeString('en-US'),
+      });
+      this.timerService.updateTimerData(this.timerData);
       clearInterval(this.currentTimer);
       this.startTimer();
-    } else if(this.timerData[len-1]?.isActive) {
+    } else if (this.timerData[len - 1]?.isActive) {
       this.pauseTimer();
     }
     this.timerControl.reset();
@@ -41,27 +43,30 @@ export class TimerMenuComponent {
 
   pauseTimer(): void {
     const len = this.timerData.length;
-    this.timerData[len-1].isActive = false;
-    this.timerData.splice(len-1,1,{
-      ...this.timerData[len-1],
+    this.timerData[len - 1].isActive = false;
+    this.timerData.splice(len - 1, 1, {
+      ...this.timerData[len - 1],
       isActive: false,
-      endTime: new Date().toLocaleDateString('en-US') +' '+ new Date().toLocaleTimeString('en-US')
-    })
-    this.timerService.updateTimerData(this.timerData)
+      endTime:
+        new Date().toLocaleDateString('en-US') +
+        ' ' +
+        new Date().toLocaleTimeString('en-US'),
+    });
+    this.timerService.updateTimerData(this.timerData);
     clearInterval(this.currentTimer);
   }
 
   startTimer(): void {
-    const len = this.timerData.length-1;
-    this.currentTimer = setInterval(()=> {
+    const len = this.timerData.length - 1;
+    this.currentTimer = setInterval(() => {
       const len = this.timerData.length;
-      this.timerData[len-1].limit--;
-      this.timerService.updateTimerData(this.timerData)
-      if(this.timerData[len-1].limit <1) {
+      this.timerData[len - 1].limit--;
+      this.timerService.updateTimerData(this.timerData);
+      if (this.timerData[len - 1].limit < 1) {
         clearInterval(this.currentTimer);
         this.pauseTimer();
       }
-    },1000)
+    }, 1000);
   }
 
   onReset(): void {
@@ -69,6 +74,6 @@ export class TimerMenuComponent {
     this.timerData = [];
     clearInterval(this.currentTimer);
     this.currentTimer = null;
-    this.timerService.updateTimerData(this.timerData)
+    this.timerService.updateTimerData(this.timerData);
   }
 }
--- src/app/route6/route6.component.ts
+++ src/app/route6/route6.component.ts
@@ -9,11 +9,15 @@ import { Component, ViewEncapsulation } from '@angular/core';
 export class Route6Component {
   items = [];
   constructor() {
+    // Creating default array for Div
     for (let i = 0; i < 100; i++) {
       this.items.push(i);
     }
   }
 
+  /*
+   * When user click on Div
+   */
   onClick(id: number) {
     id++;
     const sffx = id == 1 ? 'st' : id == 2 ? 'nd' : id == 3 ? 'rd' : 'th';
